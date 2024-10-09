67e72ed16ca3802093db6adff0dc0b59780c8535
NTCS0l1D <123yuri321@gmail.com>
2024-10-09 20:07:55 -0300

    atividade06-01

---
+++ 07-crud/src/components/Pagina.js
@@ -0,0 +1,27 @@
+'use client'
+
+export default function Pagina(props) {
+
+return (
+    <>
+    {/*barra de navegação*/}
+
+    {/*barra de titulo*/}
+    <div>
+        
+    </div>
+    {/*conteudo*/}
+    <container>
+        {props.children}
+    </container>
+    </>
+)
+
+
+
+
+
+
+
+
+}
